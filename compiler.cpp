f08fe760ccf2105b12a69590497f81023d6a8d33
Marc Page <marcallenpage@gmail.com>
2013-12-19 14:12:38 -0600

    Reorganizing error code and adding sub-block combining

--- compiler.cpp
+++ compiler.cpp
@@ -11,6 +11,7 @@ enum TokenType {
 	tPunctuation,
 	tComment,
 	tStringLiteral,
+	tBlock,
 };
 const char *typeName(TokenType type) {
 	switch(type) {
@@ -19,21 +20,11 @@ const char *typeName(TokenType type) {
 		case tPunctuation: return "Punctuation";
 		case tComment: return "Comment";
 		case tStringLiteral: return "String";
+		case tBlock: return "Block";
 		default: break;
 	}
 	return "[Unknown]";
 }
-typedef std::vector<std::string>	StringList;
-struct Token {
-	TokenType				type;
-	std::string				value;
-	std::string::size_type	position;
-	StringList				parts;
-	Token(TokenType t, const std::string &v, std::string::size_type p):type(t),value(v),position(p),parts(){
-		parts.push_back(value);
-	}
-};
-typedef std::vector<Token>	TokenList;
 class TokenException : public msg::Exception {
 	public:
 		TokenException(const std::string &message, const char *file, int line, int column) throw()
@@ -47,31 +38,86 @@ class TokenException : public msg::Exception {
 			return message+" at column: "+stream.str();
 		}
 };
-void throwLinePositionException(const std::string &message, const char * const file, const std::string &contents, const std::string::size_type position) {
-	int								line= 1;
-	std::string::size_type			sol= 0, eol, cr, lf;
-	const std::string::size_type	notFound= std::string::npos;
-
-	//printf("[[%s]]\n",contents.c_str());
-	while(sol < contents.size()) {
-		cr= contents.find('\r',sol);
-		lf= contents.find('\n',sol);
-		eol= (notFound == cr ? (notFound == lf ? contents.size() : lf) : (lf < cr ? lf : cr));
-		if( (eol == cr) && (eol + 1 < contents.size()) ) {
-			if('\n' == contents[eol+1]) {
-				++eol;
-			}
-		}
-		//printf("line=%d sol=%d cr=%d lf=%d eol=%d\n", line, static_cast<int>(sol), static_cast<int>(cr), static_cast<int>(lf), static_cast<int>(eol));
-		if( (sol <= position) && (position <= eol) ) {
-			throw TokenException(message, file, line, position - sol);
-		}
-		sol= eol+1;
-		++line;
+struct Token;
+typedef std::vector<std::string>	StringList;
+typedef std::vector<Token>			TokenList;
+struct Token {
+	TokenType				type;
+	std::string				value;
+	StringList				parts;
+	TokenList				sub;
+	const char				*filename;
+	int						line, column;
+	Token(const Token &token)
+		:type(token.type),value(token.value),parts(token.parts),sub(token.sub),filename(token.filename),line(token.line),column(token.column){}
+	Token &operator=(const Token &token) {
+		type= token.type;
+		value= token.value;
+		parts= token.parts;
+		sub= token.sub;
+		filename= token.filename;
+		line= token.line;
+		column= token.column;
+		return *this;
 	}
-	throw TokenException(message, file, line, 1);
+	void failure(const std::string &message) {
+		throw TokenException(message, filename, line, column);
+	}
+	Token(TokenType t, const char *f, const std::string &contents, std::string::size_type position, std::string::size_type count)
+			:type(t),value(contents.substr(position,count)),parts(),sub(),filename(f),line(1),column(1) {
+		parts.push_back(value);
+		std::string::size_type			sol= 0, eol, cr, lf;
+		const std::string::size_type	notFound= std::string::npos;
+
+		while(sol < contents.size()) {
+			cr= contents.find('\r',sol);
+			lf= contents.find('\n',sol);
+			eol= (notFound == cr ? (notFound == lf ? contents.size() : lf) : (lf < cr ? lf : cr));
+			if( (eol == cr) && (eol + 1 < contents.size()) ) {
+				if('\n' == contents[eol+1]) {
+					++eol;
+				}
+			}
+			if( (sol <= position) && (position <= eol) ) {
+				column= position - sol + 1;
+				return;
+			}
+			sol= eol+1;
+			++line;
+		}
+		column= contents.size() - sol + 1;
+	}
+};
+void formSubToken(TokenList::iterator &i, TokenList &tokens) {
+	TokenList::iterator	j= i+1;
+
+	while( (j != tokens.end()) && ("]" != j->value) ) {
+		if("[" == j->value) {
+			formSubToken(j, tokens);
+		}
+		i->value+=j->value;
+		i->sub.push_back(*j);
+		tokens.erase(j);
+		j= i+1;
+	}
+	if(j == tokens.end()) {
+		i->failure("Missing end bracket (])");
+	}
+	i->value+= j->value;
+	i->type= tBlock;
+	tokens.erase(j);
+}
+TokenList &formSubTokens(TokenList &tokens) {
+	printf("Subtokening\n");
+	for(TokenList::iterator i= tokens.begin(); i != tokens.end(); ++i) {
+		if("[" == i->value) {
+			formSubToken(i, tokens);
+		}
+	}
+	return tokens;
 }
 TokenList &combineTokens(TokenList &tokens) {
+	printf("Combining\n");
 	for(TokenList::iterator i= tokens.begin(); i != tokens.end(); ++i) {
 		if(tWord == i->type) {
 			TokenList::iterator	j= i+1;
@@ -105,6 +151,7 @@ TokenList &tokenize(const std::string &text, const char *filename, TokenList &to
 	const std::string spaceCharacters(" \t\r\n");
 	std::string::size_type	start= 0, end;
 
+	printf("Tokenizing\n");
 	tokens.clear();
 	while(start < text.size()) {
 		if(text[start] == '\\') {
@@ -115,24 +162,24 @@ TokenList &tokenize(const std::string &text, const char *filename, TokenList &to
 			if(std::string::npos == end) {
 				end= text.size();
 			}
-			tokens.push_back(Token(tComment,text.substr(start, end-start),start));
+			tokens.push_back(Token(tComment,filename, text, start, end-start));
 			start= end;
 		} else if(text[start] == '\'') {
 			end= text.find('\'', start+1);
 			if(std::string::npos == end) {
-				throwLinePositionException("Unterminated String", filename, text, start);
+				Token(tStringLiteral,filename, text, start, start+1).failure("Unterminated String");
 			}
-			tokens.push_back(Token(tStringLiteral,text.substr(start, end-start+1),start));
+			tokens.push_back(Token(tStringLiteral,filename, text, start, end-start+1));
 			start= end+1;
 		} else if(punctuationCharacters.find(text[start])!=std::string::npos) {
-			tokens.push_back(Token(tPunctuation,text.substr(start, 1),start));
+			tokens.push_back(Token(tPunctuation,filename, text, start, 1));
 			++start;
 		} else if(spaceCharacters.find(text[start])!=std::string::npos) {
 			end= start+1;
 			while( (end < text.size()) && (spaceCharacters.find(text[end])!=std::string::npos) ) {
 				++end;
 			}
-			tokens.push_back(Token(tWhitespace,text.substr(start, end-start),start));
+			tokens.push_back(Token(tWhitespace,filename, text, start, end-start));
 			start= end;
 		} else {
 			end= start;
@@ -140,14 +187,24 @@ TokenList &tokenize(const std::string &text, const char *filename, TokenList &to
 				++end;
 			}
 			if(start != end) {
-				tokens.push_back(Token(tWord,text.substr(start, end-start),start));
+				tokens.push_back(Token(tWord,filename, text, start, end-start));
 				start= end;
 			} else {
-				throwLinePositionException("Unknown character", filename, text, start);
+				Token(tPunctuation,filename, text, start, 1).failure("Unknown character");
 			}
 		}
 	}
 	return tokens;
+}
+void printTokens(TokenList &tokens, const std::string &indent="") {
+	for(TokenList::iterator i= tokens.begin(); i != tokens.end(); ++i) {
+		printf("%s%s@%d:%d <<%s>> ", indent.c_str(), typeName(i->type), i->line, i->column, i->value.c_str());
+		for(StringList::iterator p= i->parts.begin(); p != i->parts.end(); ++p) {
+			printf("{{%s}} ", p->c_str());
+		}
+		printf("\n");
+		printTokens(i->sub, indent+"\t");
+	}
 }
 int main(const int argc, const char *argv[]) {
 	for(int arg= 1; arg < argc; ++arg) {
@@ -156,16 +213,11 @@ int main(const int argc, const char *argv[]) {
 			TokenList	tokens;
 			std::string	buffer;
 
-			combineTokens(tokenize(file.read(buffer), argv[arg], tokens));
-			for(TokenList::iterator i= tokens.begin(); i != tokens.end(); ++i) {
-				printf("%s@%d <<%s>> ", typeName(i->type), static_cast<int>(i->position), i->value.c_str());
-				for(StringList::iterator p= i->parts.begin(); p != i->parts.end(); ++p) {
-					printf("{{%s}} ", p->c_str());
-				}
-				printf("\n");
-			}
+			formSubTokens(combineTokens(tokenize(file.read(buffer), argv[arg], tokens)));
+			printTokens(tokens);
 		} catch(const std::exception &exception) {
-			printf("EXCEPTION: path='%s' error='%s'\n", argv[arg],exception.what());
+			fprintf(stderr, "EXCEPTION: path='%s' error='%s'\n", argv[arg],exception.what());
+			return 1;
 		}
 	}
 	return 0;
